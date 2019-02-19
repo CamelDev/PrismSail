497796086bff2bc325516089fdd5ed2dce1b6ed8
Kamil Dabrowski <kada@softwaremind.com>
2019-02-19 15:28:26 +0100

    Replaced app bootstrap with Prism framework

--- PrismSail/App.xaml.cs
+++ PrismSail/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using PrismSail.Core;
 
 namespace PrismSail
 {
@@ -13,5 +14,11 @@ namespace PrismSail
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            BootStrapper bootstrapper = new BootStrapper();
+            bootstrapper.Run(); 
+        } 
     }
 }
---
+++ PrismSail/Core/Bootstrapper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Practices.Unity;
+using Prism.Modularity;
+using Prism.Unity;
+
+namespace PrismSail.Core
+{
+    class BootStrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return this.Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            App.Current.MainWindow = (Window) this.Shell;
+            App.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+            ModuleCatalog moduleCatalog = (ModuleCatalog) this.ModuleCatalog;
+            // moduleCatalog.AddModule(x);
+            // moduleCatalog.AddModule(y);
+            // moduleCatalog.AddModule(z);
+        }
+
+        protected override void ConfigureContainer()
+        {
+            // Container.RegisterInstance<>();
+            base.ConfigureContainer();
+        }
+    }
+}
