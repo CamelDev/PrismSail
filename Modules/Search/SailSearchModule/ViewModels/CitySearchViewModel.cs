3d5ff74f43a4741dd04ca96d4d265714dea9f91a
Łukasz Wielopolski <Lukasz.Wielopolski@softwaremind.com>
2019-02-20 15:27:57 +0100

    Added text box bainding and button call delegate command

---
+++ Modules/Search/SailSearchModule/ViewModels/CitySearchViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+
+namespace PrismSail.SearchModule.ViewModels
+{
+    public class CitySearchViewModel : BindableBase
+    {
+        private string _searchCity = "City...";
+        public string SearchCity
+        {
+            get => _searchCity;
+            set
+            {
+                SetProperty(ref _searchCity, value);
+                SearchCityLabel = value;
+            }
+        }
+
+        private string _searchCityLabel;
+        public string SearchCityLabel
+        {
+            get => _searchCityLabel;
+            set => SetProperty(ref _searchCityLabel, SetSearchCityLabel());
+        }
+
+        private bool _isEnabled = true;
+        public bool IsEnabled
+        {
+            get { return _isEnabled; }
+            set
+            {
+                SetProperty(ref _isEnabled, value);
+                ExecuteDelegateCommand.RaiseCanExecuteChanged();
+            }
+        }
+        public DelegateCommand ExecuteDelegateCommand { get; private set; }
+
+        public CitySearchViewModel()
+        {
+            ExecuteDelegateCommand = new DelegateCommand(SearchCityByName, CanExecute);
+        }
+
+        private void SearchCityByName()
+        {
+            SearchCityLabel = SearchCity;
+        }
+
+        private bool CanExecute()
+        {
+            return IsEnabled;
+        }
+
+        private string SetSearchCityLabel()
+        {
+            return $"Your search city: {SearchCity}";
+        }
+
+    }
+}
